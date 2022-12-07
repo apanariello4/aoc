7f8b5f7711f3f29265a84c726a3bcd56e4803d16
apanariello4 <nellopanariello96@gmail.com>
2022-12-07 22:02:45 +0100

    clean

--- 2022/01/01.py
+++ 2022/01/01.py
@@ -1,23 +1,13 @@
-sums = []
+def main(data: list[str]) -> tuple[int, int]:
+    sums = [sum([int(x) for x in chunk.split()]) for chunk in data]
+    sums = sorted(sums, reverse=True)
+    return sums[0], sum(sums[:3])
 
-with open("./input.txt") as f:
-    data = f.read().split('\n\n')
 
-tot = 0
-for chunk in data:
-    sums.append(sum([int(x) for x in chunk.split()]))
+if __name__ == "__main__":
+    with open("./input.txt") as f:
+        data = f.read().split('\n\n')
 
-sums = sorted(sums, reverse=True)
-print("part 1 ", sums[0])
-print("part 2 ", sum(sums[:3]))
-
-# with open("./input.txt", "r") as f:
-#     chunks = f.read().split('\n\n')
-# chunks = [tuple(map(int, chunk.split())) for chunk in chunks]
-# chunks.sort(key=sum, reverse=True)
-
-# ans1 = sum(chunks[0])
-# ans2 = sum(map(sum, chunks[:3]))
-
-# print(ans1)
-# print(ans2)
+    p1, p2 = main(data)
+    print("part 1 ", p1)
+    print("part 2 ", p2)
--- 2022/02/02.py
+++ 2022/02/02.py
@@ -1,32 +1,27 @@
-COMB = {"A X": 1 + 3,
-        "A Y": 2 + 6,
-        "A Z": 3 + 0,
-        "B X": 1 + 0,
-        "B Y": 2 + 3,
-        "B Z": 3 + 6,
-        "C X": 1 + 6,
-        "C Y": 2 + 0,
-        "C Z": 3 + 3, }
+COMB1 = {"A X": 1 + 3,
+         "A Y": 2 + 6,
+         "A Z": 3 + 0,
+         "B X": 1 + 0,
+         "B Y": 2 + 3,
+         "B Z": 3 + 6,
+         "C X": 1 + 6,
+         "C Y": 2 + 0,
+         "C Z": 3 + 3, }
 
-COMB_PART2 = {"A X": 3 + 0,
-              "A Y": 1 + 3,
-              "A Z": 2 + 6,
-              "B X": 1 + 0,
-              "B Y": 2 + 3,
-              "B Z": 3 + 6,
-              "C X": 2 + 0,
-              "C Y": 3 + 3,
-              "C Z": 1 + 6, }
-# A: ROCK, B: PAPER, C: SCISSORS
-# X: LOSE, Y: DRAW, Z: WIN
-# WIN = 6, DRAW = 3, LOSE = 0
+COMB2 = {"A X": 3 + 0,
+         "A Y": 1 + 3,
+         "A Z": 2 + 6,
+         "B X": 1 + 0,
+         "B Y": 2 + 3,
+         "B Z": 3 + 6,
+         "C X": 2 + 0,
+         "C Y": 3 + 3,
+         "C Z": 1 + 6, }
 
+if __name__ == "__main__":
+    with open("./input.txt") as f:
+        moves = f.read().splitlines()
 
-with open("./input.txt") as f:
-    moves = f.read().splitlines()
+    print("part 1: ", sum([COMB1[x] for x in moves]))
 
-result = sum([COMB[x] for x in moves])
-print("part 1: ", result)
-
-result = sum([COMB_PART2[x] for x in moves])
-print("part 2: ", result)
+    print("part 2: ", sum([COMB2[x] for x in moves]))
